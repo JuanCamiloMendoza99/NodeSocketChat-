da295a7f2ba26da3d73953b615f2c295813ec22c
Juan Camilo Mendoza <juan.camilo.mendoza99@gmail.com>
2025-03-24 20:59:04 -0500

    feat: configuración base con Node.js, Express y Socket.io

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import logger from "morgan";
+import { Server } from "socket.io";
+import { createServer } from "http";
+
+const port = process.env.PORT ?? 3000;
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+app.use(logger("dev"));
+
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/client/index.html");
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
